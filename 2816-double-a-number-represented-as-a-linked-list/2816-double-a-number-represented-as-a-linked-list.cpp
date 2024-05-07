62d13b76573a62ff4520cdfa1cfc51752aa23a0c
Aman Tripathi <72804550+amanboi@users.noreply.github.com>
2024-05-07 12:23:52 +0530

    Time: 185 ms (41.86%), Space: 120.8 MB (65.02%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* doubleIt(ListNode* head) {
+
+         ListNode* temp2;
+        ListNode* prev=NULL;
+        ListNode* cur = head;
+        while(cur!=NULL){  
+         temp2 =cur->next;
+        cur->next=prev;
+        prev=cur;
+        cur = temp2;
+        
+        }
+      
+        ListNode* newhead = prev;
+        ListNode* temp = newhead;
+        ListNode* previous;
+        int carry = 0;
+        while(temp!=NULL){
+          int a = (temp->val)*2+carry;
+            temp->val = a%10;
+          if(a>9){
+           
+             carry = 1;
+          }
+          else{
+            carry = 0;
+          }
+
+          previous=temp;
+          temp = temp->next;
+           
+        }
+
+        if(carry!=0){
+      ListNode* newnode = new ListNode(carry);
+       
+        newnode->next = NULL;
+        previous->next= newnode;
+        }
+
+        ListNode* temp1;
+        ListNode* prev1=NULL;
+        ListNode* cur1 = newhead;
+        while(cur1!=NULL){  
+        temp1=cur1->next;
+        cur1->next=prev1;
+        prev1=cur1;
+        cur1 = temp1;
+        
+        }
+    
+
+
+
+
+
+       return prev1;
+
+    }
+};
